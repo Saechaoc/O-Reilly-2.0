316a755d01471539a47000fdaf5f606e8bd34f02
Chris Saechao <chris_saechao@live.com>
2023-09-25 20:37:08 -0700

    Update CustomerRouters.jsx
    Imports

--- src/routers/CustomerRouters.jsx
+++ src/routers/CustomerRouters.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
-import HomePage from "/customer/components/Pages/HomePage/HomePage";
-import Cart from "/customer/components/cart/Cart";
-import ProductDetails from "/customer/components/productDetails/ProductDetails";
-import Navigation from "/customer/components/navigation/Navigation";
-import Footer from "/customer/components/Footer/Footer";
-import Checkout from "/customer/components/checkout/Checkout";
-import Order from "/customer/components/order/Order";
-import OrderDetails from "/customer/components/order/OrderDetails";
-import Product from "/customer/components/Product/Product";
+import HomePage from "../customer/components/Pages/HomePage/HomePage";
+import Cart from "../customer/components/cart/Cart";
+import ProductDetails from "../customer/components/productDetails/ProductDetails";
+import Navigation from "../customer/components/navigation/Navigation";
+import Footer from "../customer/components/Footer/Footer";
+import Checkout from "../customer/components/checkout/Checkout";
+import Order from "../customer/components/order/Order";
+import OrderDetails from "../customer/components/order/OrderDetails";
+import Product from "../customer/components/Product/Product";
 
 const CustomerRouters = () => {
   return (
