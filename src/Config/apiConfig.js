f75e8d420b1b5896f9b036d090fc841a630f034e
Chris Saechao <Chris_Saechao@live.com>
2023-10-10 09:33:40 -0700

    updating base_url

--- src/Config/apiConfig.js
+++ src/Config/apiConfig.js
@@ -1,12 +1,12 @@
-import axios from "axios"
-export const API_BASE_URL = "http://localhost:5454"
+import axios from "axios";
+export const API_BASE_URL = "https://oreilly-clone-production.up.railway.app";
 
-const jwt = localStorage.getItem("jwt")
+const jwt = localStorage.getItem("jwt");
 
 export const api = axios.create({
-    baseURL: API_BASE_URL,
-    headers: {
-        "Authorization": `Bearer ${jwt}`,
-        "Content-Type": "application/json"
-    }
-})
+  baseURL: API_BASE_URL,
+  headers: {
+    Authorization: `Bearer ${jwt}`,
+    "Content-Type": "application/json",
+  },
+});
