db8a91a8e34119704292c488bc50c70c2fc29061
Chris Saechao <Chris_Saechao@live.com>
2023-10-11 08:56:17 -0700

    removed extra brake carousels

--- src/customer/components/Pages/HomePage/HomePage.jsx
+++ src/customer/components/Pages/HomePage/HomePage.jsx
@@ -34,9 +34,6 @@ const HomePage = () => {
       <MainCarousel />
       <div className="space-y-10 py-20 flex flex-col justify-center px-5 lg:px-10">
         <HomeSectionCarousel data={brakes} sectionName={"Brakes"} />
-        <HomeSectionCarousel data={brakes} sectionName={"Brakes"} />
-        <HomeSectionCarousel data={brakes} sectionName={"Brakes"} />
-        <HomeSectionCarousel data={brakes} sectionName={"Brakes"} />
       </div>
     </div>
   );
